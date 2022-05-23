2253e668b5dd3b4700751457990ac194257fbf19
Lincoln-Caique <lincolncaique@gmail.com>
2022-05-23 07:27:06 -0300

    03 update

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import SingnIn from './pages/SignIn/ index';
 import SingnUp from './pages/SignUp/ index';
 import GlobalStyle from './styles/global';
+import AuthContext from './context/AuthContext';
 
 const App: React.FC = () => (
   <>
--- src/context/AuthContext.ts
+++ src/context/AuthContext.ts
@@ -1,12 +1,12 @@
 import { createContext } from 'react';
 import { string } from 'yup';
 
-interface AuthContext {
+interface AuthContextData {
   name: string;
 }
 
 // eslint-disable-next-line prettier/prettier
-const authContext = createContext<AuthContext>({} as AuthContext);
+const AuthContext = createContext<AuthContextData>({} as AuthContextData);
 
 
-export default authContext;
+export default AuthContext;
